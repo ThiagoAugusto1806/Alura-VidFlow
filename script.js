3b9b379b049fb38a484ce033ac410a384c6f062f
ThiagoAugusto1806 <thiagoaugusto1806@gmail.com>
2024-07-28 17:53:22 -0300

    alteração final

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function buscarEMostrarVideos(){
                     <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                     <h3 class="titulo-video">${video.titulo}}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
-                    <p class="categoria" >${video.categoria}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;  
